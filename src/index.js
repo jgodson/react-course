9c34e1dc85b719113e7b9eb6f4747b962a8f278e
Jason Godson <jason.godson@shopify.com>
2017-09-20 17:14:35 -0600

    try something different

--- src/index.js
+++ src/index.js
@@ -8,9 +8,11 @@ import App from './components/App';
 import StorePicker from './components/StorePicker';
 import NotFound from './components/NotFound';
 
+const repoName = window.location.pathname.split('/')[1];
+
 const Route = () => {
   return (
-    <BrowserRouter basename="/react-course/">
+    <BrowserRouter basename={repoName}>
       <div>
         <Match exactly pattern="/" component={StorePicker} />
         <Match exactly pattern="/store/:storeId" component={App} />
